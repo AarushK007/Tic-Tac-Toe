aba508f2806e6567b27e657929b33a8d3f5d8742
AarushK007 <aarushk1803@gmail.com>
2025-04-13 05:57:23 +0530

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -5,96 +5,64 @@ board = [
     [' ', ' ', ' ']
 ]
 
-key = [
-    ["a", [1, 1]],
-    ["b", [1, 2]],
-    ["c", [1, 3]],
-    ["d", [2, 1]],
-    ["e", [2, 2]],
-    ["f", [2, 3]],
-    ["g", [3, 1]],
-    ["h", [3, 2]],
-    ["i", [3, 3]]
-]
-
-inputs = "abcedfghi"
-
+#displaying the board
 def show():
     for j in board:
         print(j)
 
-def player1move():
-    while True:
-        player = input().lower()[0]
-        if (player not in inputs):
+#making a move
+def move(Cursor):
+    while True: #making sure the input is valid
+        player = int(input())
+        if player not in range(1, 10): #so that the input doesn't go out of the range
             print("Invalid input. Please try again.")
             continue
-        for i in key:
-            if player == i[0]:
-                index = i[1]
-        if board[index[0] - 1][index[1] - 1] != ' ':
+        import math
+        move = [(math.ceil(player / 3) - 1), ((player - 1) % 3)] #placing the x and y coordinates of the input
+        if board[move[0]][move[1]] != ' ': #so that we don't overwrite an existing slot
             print("This spot is already filled. Please try again.")
             continue
         else:
             break
-    board[int(index[0]) - 1][int(index[1]) - 1] = "X"
-    show()
-
-def player2move():
-    while True:
-        player = input().lower()[0]
-        if (player not in inputs):
-            print("Invalid input. Please try again.")
-            continue
-        for i in key:
-            if player == i[0]:
-                index = i[1]
-        if board[index[0] - 1][index[1] - 1] != ' ':
-            print("This spot is already filled. Please try again.")
-            continue
-        else:
-            break
-    board[int(index[0]) - 1][int(index[1]) - 1] = "O"
+    board[move[0]][move[1]] = "X" if (Cursor % 2 == 0) else "O" #placing X and O accordingly
     show()
 
 def wincondition():
     global winning
     winner = ""
-    winning = False
     for i in board:
-        if (i[0] == i[1] == i[2] != " "):
-            winner = i[0]
+        if (i[0] == i[1] == i[2] != " "): #checking all rows for a match
+            winner = i[0] #declaring the winning symbol
             winning = True
             break
     for i in range(3):
-        if (board[0][i] == board[1][i] == board[2][i] != " "):
-            winner = board[0][i]
+        if (board[0][i] == board[1][i] == board[2][i] != " "): #checking all columns for a match
+            winner = board[0][i] #declaring the winning symbol
             winning = True
             break
-    if ((board[0][0] == board[1][1] == board[2][2]) or (board[0][2] == board[1][1] == board[2][0]) and (board[1][1] != " ")):
-        winner = board[1][1]
+    if ((board[0][0] == board[1][1] == board[2][2]) or (board[0][2] == board[1][1] == board[2][0]) and (board[1][1] != " ")): #checking all diagonals for a match
+        winner = board[1][1] #declaring the winning symbol
     if winner == "X":
         print("Player 1 is the winner.")
     elif winner == "O":
         print("Player 2 is the winner.")
-    elif counter == 9:
+    elif counter == 9: #if the board is full without any winners
         print("It's a draw.")
         winning = "Draw"
 
 counter = 0
+Cursor = 0
+winning = False
+
 print("""This is the board:
-[a, b, c],
-[d, e, f],
-[g, h, i]""")
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]""")
 
 while True:
-    player1move()
+    move(Cursor)
     counter += 1
     wincondition()
     if winning != False:
         break
-    player2move()
-    counter += 1
-    wincondition()
-    if winning != False:
-        break
+    Cursor += 1
